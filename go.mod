4f491b59c9b72bbf714e061569602f3d0f76c729
pongpatapee <pongpatapee1021@gmail.com>
2024-06-30 11:20:12 -0400

    feat: finished game

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module number_slider_puzzel
 
 go 1.22.4
+
+require (
+	github.com/eiannone/keyboard v0.0.0-20220611211555-0d226195f203 // indirect
+	golang.org/x/sys v0.21.0 // indirect
+)
--- main.go
+++ main.go
@@ -2,7 +2,10 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
 	"strings"
+
+	"github.com/eiannone/keyboard"
 )
 
 type Board [][]string
@@ -50,17 +53,130 @@ func printBoard(board Board) {
 
 	for r := range len(board) {
 		row = "|"
-		for c := range len(board) {
+		for c := range len(board[r]) {
 			row += fmt.Sprintf(" %3v |", board[r][c])
 		}
 		boardStr += generateBorder(len(row)) + "\n" + row + "\n"
 	}
 	boardStr += generateBorder(len(row)) + "\n"
 
+	// clear screen
+	fmt.Print("\033[H\033[2J")
+	fmt.Println("Press ESC or q to quit")
 	fmt.Print(boardStr)
 }
 
+func randomizeBoard(board Board) {
+	for range 100 {
+		r1, c1 := rand.Intn(len(board)), rand.Intn(len(board))
+		r2, c2 := rand.Intn(len(board)), rand.Intn(len(board))
+
+		board[r1][c1], board[r2][c2] = board[r2][c2], board[r1][c1]
+	}
+}
+
+func getEmptyPos(board Board) (int, int) {
+	for r := range len(board) {
+		for c := range len(board[r]) {
+			if board[r][c] == "" {
+				return r, c
+			}
+		}
+	}
+
+	return -1, -1
+}
+
+func move(board Board, dir string) {
+	er, ec := getEmptyPos(board)
+	r, c := -1, -1
+
+	switch dir {
+	case "U":
+		// grab element from below to swap with
+		r, c = er+1, ec
+
+		if r >= len(board) {
+			return
+		}
+	case "D":
+		// grab element from above to swap with
+		r, c = er-1, ec
+		if r < 0 {
+			return
+		}
+	case "L":
+		// grab element from the right to swap with
+		r, c = er, ec+1
+		if c >= len(board[r]) {
+			return
+		}
+	case "R":
+		// grab element from the left to swap with
+		r, c = er, ec-1
+		if c < 0 {
+			return
+		}
+	}
+
+	board[r][c], board[er][ec] = board[er][ec], board[r][c]
+}
+
+func isSolved(board Board, solvedBoard Board) bool {
+	for r := range len(board) {
+		for c := range len(board[r]) {
+			if board[r][c] != solvedBoard[r][c] {
+				return false
+			}
+		}
+	}
+
+	return true
+}
+
 func main() {
-	board := createBoard(5)
-	printBoard(board)
+	size := 3
+	board := createBoard(size)
+	solvedBoard := createBoard(size)
+	randomizeBoard(board)
+
+	if err := keyboard.Open(); err != nil {
+		panic(err)
+	}
+	defer func() {
+		_ = keyboard.Close()
+	}()
+
+	for {
+
+		// Game loop
+		printBoard(board)
+
+		char, key, err := keyboard.GetKey()
+		if err != nil {
+			panic(err)
+		}
+		// fmt.Printf("You pressed: rune %q, key %X\r\n", char, key)
+		if key == keyboard.KeyEsc || char == 'q' {
+			break
+		}
+
+		switch char {
+		case 'w':
+			move(board, "U")
+		case 'a':
+			move(board, "L")
+		case 's':
+			move(board, "D")
+		case 'd':
+			move(board, "R")
+		}
+
+		if isSolved(board, solvedBoard) {
+			printBoard(board)
+			fmt.Printf("You solved %v x %v board!\n", size, size)
+			break
+		}
+
+	}
 }
